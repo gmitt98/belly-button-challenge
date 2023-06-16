c43a90a99dfa4aa72c11b46d102ac177ffc81be3
Galen <gmitt98@users.noreply.github.com>
2023-06-16 13:08:11 -0700

    Update app.js

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -14,21 +14,19 @@ d3.json(url).then(function(data) {
     // Put the list of IDs in the dropdown
     // From the HTML, I can see that we have id="selDataset" for this box
     let MyMenu = d3.select("#selDataset"); // Select the html element and assign it to a variable for the dropdown menu
+    MyMenu.append("option").text('select an ID')
     data.names.forEach((name) => {
         MyMenu.append("option").text(name).property("value", name); // Now the list of names is in the HTML element
     });
-    
-    // Create a chart that we will update with actual data, we start it with the first Subject ID that we have (940 in this case)
-    //updateCharts(data.names[0], data);
 });
 
 // Function that will update the chart, it takes the Subject ID and the data as arguments
-function updateCharts(id, data) {
+function updateBarChart(id, data) {
     let sample = data.samples.filter(sample => sample.id === id)[0]; // Filters the data to just the sample we want - matching the ID selected
     console.log(sample) // Checking that this is the right sample each time
-    let otu_ids = sample.otu_ids.slice(0, 10).map(otu_id => `OTU ${otu_id}`) //.reverse(); // Gets the top 10 OTUs
-    let sample_values = sample.sample_values.slice(0, 10) //.reverse(); // Gets the values for those top 10 OTUs
-    let otu_labels = sample.otu_labels.slice(0, 10) //.reverse(); // Gets the labels for the top 10 OTUs
+    let otu_ids = sample.otu_ids.slice(0, 10).map(otu_id => `OTU ${otu_id}`).reverse(); // Gets the top 10 OTUs - reverse to make descending order
+    let sample_values = sample.sample_values.slice(0, 10).reverse(); // Gets the values for those top 10 OTUs
+    let otu_labels = sample.otu_labels.slice(0, 10).reverse(); // Gets the labels for the top 10 OTUs
     // Create the trace for the chart
     let trace = {
         x: sample_values,
@@ -45,11 +43,38 @@ function updateCharts(id, data) {
     Plotly.newPlot("bar", [trace], layout);
 }
 
+
+// Function that will update the bubble chart
+function updateBubbleChart(id, data) {
+    let sample = data.samples.filter(sample => sample.id === id)[0]; // Filters the data to just the sample we want - matching the ID selected
+    console.log(sample) // Checking that this is the right sample each time
+    let otu_ids = sample.otu_ids.slice(0, 10).map(otu_id => `OTU ${otu_id}`).reverse(); // Gets the top 10 OTUs - reverse to make descending order
+    let sample_values = sample.sample_values.slice(0, 10).reverse(); // Gets the values for those top 10 OTUs
+    let otu_labels = sample.otu_labels.slice(0, 10).reverse(); // Gets the labels for the top 10 OTUs
+    // Trace for the bubble chart
+    let trace2 = {
+        x : otu_ids,
+        y : sample_values,
+        mode : 'markers',
+        marker: {
+            size: sample_values,
+            color: otu_ids,
+            colorscale: 'peach'
+        },
+        text: otu_labels,
+        type: "bubble"
+    };
+    let layout2 = {
+        title: 'Bubble chart'
+    };
+    Plotly.newPlot("bubble", [trace2], layout2)
+};
+
 // Update chart when new dropdown option is selected
 d3.selectAll("#selDataset").on("change", function() { // When the dropdown list is changed, i.e. new option selected...
-    let newId = d3.select(this).property("value"); // Get the Subject ID from the 'value' off the dropdown list
-    
+    let newId = d3.select(this).property("value"); // Get the Subject ID from the 'value' off the dropdown list 
     d3.json(url).then(function(data) {
-        updateCharts(newId, data); // Run the update function we wrote
+        updateBarChart(newId, data), // Run the update function we wrote
+        updateBubbleChart(newId, data)
     });
 });
